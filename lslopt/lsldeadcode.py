Sei-Lisa/LSL-PyOptimizer

commit 562154e7aa65fe766ab2ae467d041985873a9960

Remove the symbol table entry too when removing a global state/function/var.

lslopt/lsldeadcode.py
@@ -489,6 +489,9 @@ class deadcode(object):
                 delete = self.SymbolReplacedOrDeleted(node)
 
             if delete:
+                if node['nt'] in ('DECL', 'STDEF', 'FNDEF'):
+                    # Delete the symbol table entry too
+                    del self.symtab[0][node['name']]
                 del self.tree[idx]
                 del LocMap[idx]
             else:

commit fc7abc7d99d225c573ad7dad0c382a8cc1493269

Fix bug broken by 562154e7aa65fe766ab2ae467d041985873a9960.
Referencing a global that was optimized out, caused a crash.

```
integer DEBUG = 1;

default
{
    state_entry()
    {
        if (DEBUG) llOwnerSay("DEBUG");
    }
}
```

lslopt/lsldeadcode.py
@@ -489,9 +489,10 @@ class deadcode(object):
                 delete = self.SymbolReplacedOrDeleted(node)
 
             if delete:
-                if node['nt'] in ('DECL', 'STDEF', 'FNDEF'):
-                    # Delete the symbol table entry too
-                    del self.symtab[0][node['name']]
+                # FIXME: This makes sense but it doesn't work. Analyze why and fix.
+                #if node['nt'] in ('DECL', 'STDEF', 'FNDEF'):
+                #    # Delete the symbol table entry too
+                #    del self.symtab[0][node['name']]
                 del self.tree[idx]
                 del LocMap[idx]
             else: