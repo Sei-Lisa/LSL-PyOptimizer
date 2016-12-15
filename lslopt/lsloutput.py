Sei-Lisa/LSL-PyOptimizer

commit 397dc896fbcd13f08dec173c85a9a1959d73302c

Fix --optimizer-options=-optimize still applying some optimizations.
This option normally takes effect through the base class in lsloptimize.py, which doesn't call the optimization techniques if deactivated. However, lsloutput.py is not called by it, and it applied some optimizations on its own.

Fixed on the reading of the optimization options, by filtering them by whether the optimize option is active.

lslopt/lsloutput.py
@@ -437,9 +437,12 @@ class outscript(object):
         self.tree, self.symtab = treesymtab
 
         # Grab options
-        self.optsigns = 'optsigns' in options
-        self.optfloats = 'optfloats' in options
-        self.foldconst = 'constfold' in options
+        self.optimize = 'optimize' in options
+        # These are optimization options that depend on the above:
+        self.optsigns = self.optimize and 'optsigns' in options
+        self.optfloats = self.optimize and 'optfloats' in options
+        self.foldconst = self.optimize and 'constfold' in options
+
         self.warntabs = 'warntabs' in options
 
         ret = ''

commit 7fa691ead70109e70307384bdfba11bc342b93bb

Activate 'optimize' option by default in lsloutput.
Failure to do so caused a regression test to fail. Harmless, because that option is overriden by main, but fixed.

Bug was introduced in commit 397dc89, with the requirement that the 'optimize' option be active for output optimizations to be applied, by forgetting to update the function header to add that default option.

lslopt/lsloutput.py
@@ -432,7 +432,8 @@ class outscript(object):
 
         assert False, "Internal error: node type not handled: " + nt # pragma: no cover
 
-    def output(self, treesymtab, options = ('optsigns','optfloats','warntabs')):
+    def output(self, treesymtab, options = ('optimize',
+            'optsigns','optfloats','warntabs')):
         # Build a sorted list of dict entries
         self.tree, self.symtab = treesymtab
 