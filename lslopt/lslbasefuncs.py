6591b5dfd8c581115a65bacfb052979af14ed330
Sei Lisa <sei-lisa@email.fake>
2017-01-17 02:22:10 +0100

    Fix return type of cross product.
    mod(Vector, Vector) was returning tuple, not Vector.

--- lslopt/lslbasefuncs.py
+++ lslopt/lslbasefuncs.py
@@ -730,7 +730,9 @@ def mod(a, b, f32=True):
         # cross product
         a = v2f(a)
         b = v2f(b)
-        return F32((a[1]*b[2]-a[2]*b[1], a[2]*b[0]-a[0]*b[2], a[0]*b[1]-a[1]*b[0]), f32)
+        return Vector(F32((a[1]*b[2]-a[2]*b[1],
+                           a[2]*b[0]-a[0]*b[2],
+                           a[0]*b[1]-a[1]*b[0]), f32))
 
     raise ELSLTypeMismatch
 
